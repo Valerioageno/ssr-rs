8146b20cb0adb16bc77810a8467170e430843daa
Valerio <valerioageno@yahoo.it>
2021-04-25 15:59:39 +0200

    fmt project and add logo

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,7 +1,7 @@
 #[allow(unused_imports)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ssr_rs::Ssr;
 use serde_json;
+use ssr_rs::Ssr;
 
 pub fn bench_render_to_string_no_params(c: &mut Criterion) {
     c.bench_function("render_to_string_no_params", |b| {
@@ -10,7 +10,6 @@ pub fn bench_render_to_string_no_params(c: &mut Criterion) {
 }
 
 pub fn bench_render_to_string_with_params(c: &mut Criterion) {
-
     let mock_props = r##"{
         "params": [
             "hello",
@@ -21,16 +20,8 @@ pub fn bench_render_to_string_with_params(c: &mut Criterion) {
 
     let json = serde_json::to_string(&mock_props).unwrap();
 
-
     c.bench_function("render_to_string_with_params", |b| {
-        b.iter(|| {
-            Ssr::render_to_string(
-                "./client/dist/ssr/index.js",
-                "SSR",
-                "Index",
-                Some(&json),
-            )
-        })
+        b.iter(|| Ssr::render_to_string("./client/dist/ssr/index.js", "SSR", "Index", Some(&json)))
     });
 }
 
