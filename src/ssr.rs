7462ea23f02e4126e76f567a1681e110fe82853f
Valerio <valerioageno@yahoo.it>
2021-09-26 21:24:59 +0200

    update rusty_v8 to v0.30.0

--- src/ssr.rs
+++ src/ssr.rs
@@ -8,7 +8,7 @@ impl Ssr {
         lazy_static! {
           static ref INIT_PLATFORM: () = {
               //Initialize a new V8 platform
-              let platform = v8::new_default_platform().unwrap();
+              let platform = v8::new_default_platform(0, false).make_shared();
               v8::V8::initialize_platform(platform);
               v8::V8::initialize();
           };
