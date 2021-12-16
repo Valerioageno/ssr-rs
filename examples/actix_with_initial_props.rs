a0ddf37942a380081c7aae3d6d490dd75cef1d31
Ryan Swart <ryan@talo.systems>
2021-12-16 17:43:47 +0800

    keep v8 contexts alive

--- examples/actix_with_initial_props.rs
+++ examples/actix_with_initial_props.rs
@@ -1,5 +1,6 @@
-use actix_web::{get, http::StatusCode, App, HttpResponse, HttpServer};
-use std::fs::read_to_string;
+use std::sync::Mutex;
+
+use actix_web::{get, http::StatusCode, middleware::Logger, web, App, HttpResponse, HttpServer};
 
 use actix_files as fs;
 
@@ -7,22 +8,39 @@ use ssr_rs::Ssr;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    std::env::set_var(
+        "RUST_LOG",
+        "actix_example=debug,actix_web=debug,actix_http=debug,actix_service=debug",
+    );
+    env_logger::init();
+    const source: &str = include_str!("../client/dist/ssr/index.js");
+    let entry_point = "SSR".into();
+    let (ssr, receiver) = Ssr::new(source, entry_point);
+    let issr = ssr.clone();
+
+    let server = HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(issr.clone()))
+            .wrap(Logger::default())
             .service(fs::Files::new("/styles", "client/dist/ssr/styles/").show_files_listing())
             .service(fs::Files::new("/images", "client/dist/ssr/images/").show_files_listing())
             .service(fs::Files::new("/scripts", "client/dist/client/").show_files_listing())
             .service(index)
     })
     .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    .run();
+
+    futures::try_join!(server, async {
+        ssr.clone()
+            .listen(receiver)
+            .await
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, "test"))
+    })
+    .map(|_| ())
 }
 
 #[get("/")]
-async fn index() -> HttpResponse {
-    let source = read_to_string("./client/dist/ssr/index.js").unwrap();
-
+async fn index(ssr: web::Data<Ssr<'_>>) -> Result<HttpResponse, Box<dyn std::error::Error>> {
     let mock_props = r##"{
         "params": [
             "hello",
@@ -38,7 +56,8 @@ async fn index() -> HttpResponse {
     //     Some(&mock_props),
     // ))));
 
-    HttpResponse::build(StatusCode::OK)
+    let body = ssr.render_to_string(Some(&mock_props)).await?;
+    Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(Ssr::render_to_string(&source, "SSR", Some(&mock_props)))
+        .body(body))
 }
--- src/ssr.rs
+++ src/ssr.rs
@@ -1,9 +1,88 @@
+use futures::channel::oneshot::{Receiver, Sender};
 use rusty_v8 as v8;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Mutex};
+use v8::{Context, HandleScope};
 
-pub struct Ssr {}
+#[derive(Clone)]
+pub struct Ssr<'a> {
+    inbox: std::sync::mpsc::Sender<(String, Sender<String>)>,
+    source: &'a str,
+    entry_point: &'a str,
+}
 
-impl Ssr {
+impl<'a> Ssr<'a> {
+    pub async fn listen(
+        self,
+        reciever: std::sync::mpsc::Receiver<(String, Sender<String>)>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        //The isolate represents an isolated instance of the v8 engine
+        //Object from one isolate must not be used in other isolates.
+        let mut isolate: v8::OwnedIsolate = v8::Isolate::new(Default::default());
+
+        //A stack-allocated class that governs a number of local handles.
+        let mut handle_scope: HandleScope<()> = v8::HandleScope::new(&mut isolate);
+
+        //A sandboxed execution context with its own set of built-in objects and functions.
+        let context: v8::Local<Context> = v8::Context::new(&mut handle_scope);
+
+        //Stack-allocated class which sets the execution context for all operations executed within a local scope.
+        let mut scope = v8::ContextScope::new(&mut handle_scope, context);
+
+        let code = v8::String::new(&mut scope, &format!("{};{}", self.source, self.entry_point))
+            .expect("Strings are needed");
+
+        let script =
+            v8::Script::compile(&mut scope, code, None).expect("There aren't runnable scripts");
+
+        let exports = script
+            .run(&mut scope)
+            .expect("Missing entry point. Is the bundle exported as a variable?");
+
+        let object = exports.to_object(&mut scope).expect("There are no objects");
+        let fn_map = Self::create_fn_map(&mut scope, object);
+        Ok(while let (params, out) = reciever.recv().unwrap() {
+            let params: v8::Local<v8::Value> = match v8::String::new(&mut scope, &params) {
+                Some(s) => s.into(),
+                None => v8::undefined(&mut scope).into(),
+            };
+
+            let undef = v8::undefined(&mut scope).into();
+
+            let mut rendered = String::new();
+
+            for key in fn_map.keys() {
+                let result = fn_map[key]
+                    .call(&mut scope, undef, &[params])
+                    .expect("Are provided all needed props?");
+
+                let result = result.to_string(&mut scope).unwrap();
+
+                rendered = format!("{}{}", rendered, result.to_rust_string_lossy(&mut scope));
+            }
+            out.send(rendered).unwrap();
+        })
+    }
+
+    pub fn new(
+        source: &'a str,
+        entry_point: &'a str,
+    ) -> (Self, std::sync::mpsc::Receiver<(String, Sender<String>)>) {
+        Self::init_platform();
+        let (inbox, inbox_out): (
+            std::sync::mpsc::Sender<(String, Sender<String>)>,
+            std::sync::mpsc::Receiver<(String, Sender<String>)>,
+        ) = std::sync::mpsc::channel();
+
+        (
+            Ssr {
+                inbox,
+                source,
+                entry_point,
+            },
+            inbox_out,
+        )
+    }
+
     fn init_platform() {
         lazy_static! {
           static ref INIT_PLATFORM: () = {
@@ -23,57 +102,16 @@ impl Ssr {
     /// <a href="https://github.com/Valerioageno/ssr-rs/blob/main/examples/actix_with_initial_props.rs" target="_blank">Here</a> an useful example of how to use initial params with the actix framework.
     ///
     /// "enrty_point" is the variable name set from the frontend bundler used. <a href="https://github.com/Valerioageno/ssr-rs/blob/main/client/webpack.ssr.js" target="_blank">Here</a> an example from webpack.
-    pub fn render_to_string(source: &str, entry_point: &str, params: Option<&str>) -> String {
-        Self::init_platform();
-
+    pub async fn render_to_string(
+        &self,
+        params: Option<&str>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
         {
-            //The isolate rapresente an isolated instance of the v8 engine
-            //Object from one isolate must not be used in other isolates.
-            let isolate = &mut v8::Isolate::new(Default::default());
+            let (send, recieve) = futures::channel::oneshot::channel();
+            let sender = &self.inbox;
+            sender.send((params.unwrap_or("").to_string(), send))?;
 
-            //A stack-allocated class that governs a number of local handles.
-            let handle_scope = &mut v8::HandleScope::new(isolate);
-
-            //A sandboxed execution context with its own set of built-in objects and functions.
-            let context = v8::Context::new(handle_scope);
-
-            //Stack-allocated class which sets the execution context for all operations executed within a local scope.
-            let scope = &mut v8::ContextScope::new(handle_scope, context);
-
-            let code = v8::String::new(scope, &format!("{};{}", source, entry_point))
-                .expect("Strings are needed");
-
-            let script =
-                v8::Script::compile(scope, code, None).expect("There aren't runnable scripts");
-
-            let exports = script
-                .run(scope)
-                .expect("Missing entry point. Is the bundle exported as a variable?");
-
-            let object = exports.to_object(scope).expect("There are no objects");
-
-            let fn_map = Self::create_fn_map(scope, object);
-
-            let params: v8::Local<v8::Value> = match v8::String::new(scope, params.unwrap_or("")) {
-                Some(s) => s.into(),
-                None => v8::undefined(scope).into(),
-            };
-
-            let undef = v8::undefined(scope).into();
-
-            let mut rendered = String::new();
-
-            for key in fn_map.keys() {
-                let result = fn_map[key]
-                    .call(scope, undef, &[params])
-                    .expect("Are provided all needed props?");
-
-                let result = result.to_string(scope).unwrap();
-
-                rendered = format!("{}{}", rendered, result.to_rust_string_lossy(scope));
-            }
-
-            rendered
+            Ok(recieve.await?)
         }
     }
 
