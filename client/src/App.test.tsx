7903ea6a51b2bddb72c659d1645cafb2875674e9
Valerio Ageno <valerioageno@yahoo.it>
2023-12-31 11:20:05 +0100

    feat: update dependencies

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,16 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 const props = {
-  params: [
-    "first param",
-    "second param",
-    "third param"
-  ]
-}
-
+  params: ['first param', 'second param', 'third param'],
+};
 
 test('renders learn react link', () => {
   render(<App {...props} />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInstanceOf(HTMLElement);
 });
