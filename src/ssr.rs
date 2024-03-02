7666036011fd914a5a23c4295883fa861a07a3de
Valerio Ageno <valerioageno@yahoo.it>
2024-03-02 12:22:30 +0100

    fix: prevent clippy errors

--- src/ssr.rs
+++ src/ssr.rs
@@ -27,7 +27,7 @@ impl<'s, 'i> Ssr<'s, 'i>
 where
     's: 'i,
 {
-    pub fn create_platform() -> () {
+    pub fn create_platform() {
         let platform = v8::new_default_platform(0, false).make_shared();
         v8::V8::initialize_platform(platform);
         v8::V8::initialize();
@@ -44,31 +44,31 @@ where
         let scope_ptr =
             unsafe { Box::into_raw(Box::new(v8::ContextScope::new(&mut *handle_scope, context))) };
 
-        let mut scope = unsafe { &mut *scope_ptr };
+        let scope = unsafe { &mut *scope_ptr };
 
-        let code = v8::String::new(&mut scope, &format!("{source};{entry_point}"))
+        let code = v8::String::new(scope, &format!("{source};{entry_point}"))
             .expect("Invalid JS: Strings are needed");
 
-        let script = v8::Script::compile(&mut scope, code, None)
+        let script = v8::Script::compile(scope, code, None)
             .expect("Invalid JS: There aren't runnable scripts");
 
         let exports = script
-            .run(&mut scope)
+            .run(scope)
             .expect("Invalid JS: Missing entry point. Is the bundle exported as a variable?");
 
         let object = exports
-            .to_object(&mut scope)
+            .to_object(scope)
             .expect("Invalid JS: There are no objects");
 
         let mut fn_map: HashMap<String, v8::Local<v8::Function>> = HashMap::new();
 
-        if let Some(props) = object.get_own_property_names(&mut scope, Default::default()) {
+        if let Some(props) = object.get_own_property_names(scope, Default::default()) {
             fn_map = Some(props)
                 .iter()
                 .enumerate()
                 .map(|(i, &p)| {
                     let name = p
-                        .get_index(&mut scope, i as u32)
+                        .get_index(scope, i as u32)
                         .expect("Failed to get function name");
 
                     let mut scope = v8::EscapableHandleScope::new(scope);
@@ -100,28 +100,28 @@ where
 
     /// Execute the Javascript functions and return the result as string.
     pub fn render_to_string(&mut self, params: Option<&str>) -> String {
-        let mut scope = unsafe { &mut *self.scope };
+        let scope = unsafe { &mut *self.scope };
 
-        let params: v8::Local<v8::Value> = match v8::String::new(&mut scope, params.unwrap_or("")) {
+        let params: v8::Local<v8::Value> = match v8::String::new(scope, params.unwrap_or("")) {
             Some(s) => s.into(),
-            None => v8::undefined(&mut scope).into(),
+            None => v8::undefined(scope).into(),
         };
 
-        let undef = v8::undefined(&mut scope).into();
+        let undef = v8::undefined(scope).into();
 
         let mut rendered = String::new();
 
         // TODO: transform this into an iterator
         for key in self.fn_map.keys() {
             let result = self.fn_map[key]
-                .call(&mut scope, undef, &[params])
+                .call(scope, undef, &[params])
                 .expect("Failed to call function");
 
             let result = result
-                .to_string(&mut scope)
+                .to_string(scope)
                 .expect("Failed to parse the result to string");
 
-            rendered = format!("{}{}", rendered, result.to_rust_string_lossy(&mut scope));
+            rendered = format!("{}{}", rendered, result.to_rust_string_lossy(scope));
         }
 
         rendered
