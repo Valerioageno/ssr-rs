f4cc8e9811ddb6f5a4524e54cc23315d8adb7d37
musaabgodil <hello@musaab.com>
2025-03-16 17:49:56 -0400

    test: iife output and test for expected bundle from svelte 5

--- tests/svelte.rs
+++ tests/svelte.rs
@@ -22,3 +22,22 @@ fn renders_svelte_exported_as_iife() {
 
     assert_eq!(html, "<div></div>");
 }
+
+#[test]
+fn renders_svelte_5_exported_as_iife() {
+    prepare();
+
+    let source = read_to_string("./tests/assets/svelte-5-iife.js").unwrap();
+
+    let mut js = Ssr::from(source, "").unwrap();
+
+    let result = js.render_to_string(None).unwrap();
+    let html_obj: serde_json::Value = serde_json::from_str(&result).unwrap();
+
+    let head = html_obj["head"].as_str().unwrap_or("");
+    let body = html_obj["body"].as_str().unwrap_or("");
+
+    assert_eq!(head, "");
+    // With SSR, Svelte 5 generates comments along with rendered output to track hydration internally
+    assert_eq!(body, "<!--[--><div></div><!--]-->");
+}
