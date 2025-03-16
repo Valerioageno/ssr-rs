7d859ce1962c5881ac6b4eb72542415deac3aa94
musaabgodil <hello@musaab.com>
2025-03-16 17:54:51 -0400

    chore: rm after updates to original vite-svelte example

--- examples/vite-svelte-5/frontend/main.js
+++
@@ -1,8 +0,0 @@
-import { hydrate } from "svelte";
-
-import App from "./App.svelte";
-import "./app.css";
-
-hydrate(App, {
-  target: document.querySelector("#svelte-app"),
-});
--- examples/vite-svelte-5/frontend/server.js
+++
@@ -1,7 +0,0 @@
-import { render as renderer } from "svelte/server";
-import App from "./App.svelte";
-
-export function render() {
-  const { head, body } = renderer(App);
-  return JSON.stringify({ head, body });
-}
