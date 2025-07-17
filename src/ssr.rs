db68d6b2e9009b41f11453bed89e8d3fa026f724
Darric Heng <darricheng@gmail.com>
2025-07-17 23:09:30 +0800

    Update Svelte version in docs (#57)
    * Update Svelte version in docs
    
    * chore: run clippy fix

--- src/ssr.rs
+++ src/ssr.rs
@@ -15,7 +15,7 @@ pub enum SsrError {
 
 impl fmt::Display for SsrError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
